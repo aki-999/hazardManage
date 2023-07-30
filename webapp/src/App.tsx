b5043386b43a30a7c26089dd3029cc6e28f1a0ce
aki-999 <a6tklm@gmail.com>
2023-07-30 21:17:25 +0900

    add sample

---
+++ webapp/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import * as UI from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UI.AppBar>
+          <UI.Typography>abc</UI.Typography>
+        </UI.AppBar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
